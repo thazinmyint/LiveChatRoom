f8a410896de733738e7227714e447dfaece00616
ThaZinMyint <tm129105@gmail.com>
2021-10-13 18:52:17 +0630

    firebase setup (connecting many frontends to single backend project

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+import "firebase/firestore"
+const firebaseConfig = {
+    apiKey: "AIzaSyDhkc2hUo-JqtXZ1mf54GDRSHeoAk9gRec",
+    authDomain: "vue-blog-system-8cb78.firebaseapp.com",
+    projectId: "vue-blog-system-8cb78",
+    storageBucket: "vue-blog-system-8cb78.appspot.com",
+    messagingSenderId: "967811995405",
+    appId: "1:967811995405:web:066e6503b2c80f7ac84244"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  let db=firebase.firestore();
