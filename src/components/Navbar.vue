349ff49756632d8defa87f36b043702b6a032ea8
ThaZinMyint <tm129105@gmail.com>
2021-10-14 20:08:20 +0630

    getting the current user (composable)

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,8 @@
 <template>
-  <nav>
+  <nav v-if="user">
       <div>
-          <p>Hi display name</p>
-          <p class="email">logged in as email</p>
+          <p>Hi {{user.displayName}}</p>
+          <p class="email">logged in as {{user.email}}</p>
       </div>
       <button @click="logout">Logout</button>
   </nav>
@@ -10,10 +10,12 @@
 
 <script>
 import { ref } from '@vue/reactivity';
-import {auth} from '../firebase/config';
+
+import getUser from "../composables/getUser"
 export default {
     setup(){
         let error=ref(null);
+        let {user}=getUser();
         let logout=async()=>{
            try {
                 await auth.signOut();
@@ -24,7 +26,9 @@ export default {
                console.log(err.value)
            }
         }
-        return {logout};
+
+        
+        return {logout,user};
     }
 }
 </script>
