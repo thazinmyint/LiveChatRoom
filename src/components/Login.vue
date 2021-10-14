93a945374cbd18bf15fe3045290fd41b7d450257
ThaZinMyint <tm129105@gmail.com>
2021-10-14 16:45:14 +0630

    event emit in setup function(redirect to chatroom

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -12,7 +12,7 @@
 import { ref } from '@vue/reactivity'
 import useLogin from '../composables/useLogin'
 export default {
-    setup(){
+    setup(props,context){
         let email=ref("")
         let password=ref("")
         let {error,signIn}=useLogin(); 
@@ -20,8 +20,7 @@ export default {
         let login=async()=>{
           let res=await signIn(email.value,password.value);
           if(res){
-            console.log(res.user)
-
+              context.emit("enterChatroom")
           }
         }
 
