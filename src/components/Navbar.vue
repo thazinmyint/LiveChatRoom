43e4417a37c822c59bfabae22cf11e9821763bf1
ThaZinMyint <tm129105@gmail.com>
2021-10-14 19:10:28 +0630

    create navbar component and make logout button

---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav>
+      <div>
+          <p>Hi display name</p>
+          <p class="email">logged in as email</p>
+      </div>
+      <button @click="logout">Logout</button>
+  </nav>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity';
+import {auth} from '../firebase/config';
+export default {
+    setup(){
+        let error=ref(null);
+        let logout=async()=>{
+           try {
+                await auth.signOut();
+                console.log('user logged out');
+               
+           } catch (err) {
+               error.value=err.message;
+               console.log(err.value)
+           }
+        }
+        return {logout};
+    }
+}
+</script>
+
+<style>
+
+</style>
